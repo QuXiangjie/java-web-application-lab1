57bf6a7040b8ae8a9fa71e3daef2344414a3624f
Xiangjie Qu <xqu@gus.pittstate.edu>
2023-01-30 10:29:46 -0600

    new class

---
+++ src/domain/Account.java
@@ -0,0 +1,116 @@
+
+package domain;
+
+
+public class Account {
+    private int accountNumber;
+    private int customerID;   //this is a foreign from customer!!! no solve now
+    private String accountName;
+    private String dateOpened;
+    public Account(int accountNumber, int customerID, String accountName, String dateOpened) {
+        this.accountNumber = accountNumber;
+        this.customerID = customerID;
+        this.accountName = accountName;
+        this.dateOpened = dateOpened;
+    }
+
+    public int getAccountNumber() {
+        return accountNumber;
+    }
+
+    public void setAccountNumber(int accountNumber) {
+        this.accountNumber = accountNumber;
+    }
+
+    public int getCustomerID() {
+        return customerID;
+    }
+
+    public void setCustomerID(int customerID) {
+        this.customerID = customerID;
+    }
+
+    public String getAccountName() {
+        return accountName;
+    }
+
+    public void setAccountName(String accountName) {
+        this.accountName = accountName;
+    }
+
+    public String getDateOpened() {
+        return dateOpened;
+    }
+
+    public void setDateOpened(String dateOpened) {
+        this.dateOpened = dateOpened;
+    }
+    
+}
+class AssetAccount extends Account {
+    private double interestRate;
+    private double transactionFee;
+
+    public AssetAccount(int accountNumber, int customerID, String accountName, String dateOpened,
+                        double interestRate, double transactionFee) {
+        super(accountNumber, customerID, accountName, dateOpened);
+        this.interestRate = interestRate;
+        this.transactionFee = transactionFee;
+    }
+
+    public double getInterestRate() {
+        return interestRate;
+    }
+
+    public void setInterestRate(double interestRate) {
+        this.interestRate = interestRate;
+    }
+
+    public double getTransactionFee() {
+        return transactionFee;
+    }
+
+    public void setTransactionFee(double transactionFee) {
+        this.transactionFee = transactionFee;
+    }
+}
+
+class LiabilityAccount extends Account {
+    private double interestRate;
+    private double monthlyPayment;
+    private String paymentDate;
+
+    public LiabilityAccount(int accountNumber, int customerID, String accountName, String dateOpened,
+                            double interestRate, double monthlyPayment, String paymentDate) {
+        super(accountNumber, customerID, accountName, dateOpened);
+        this.interestRate = interestRate;
+        this.monthlyPayment = monthlyPayment;
+        this.paymentDate = paymentDate;
+    }
+
+    public double getInterestRate() {
+        return interestRate;
+    }
+
+    public void setInterestRate(double interestRate) {
+        this.interestRate = interestRate;
+    }
+
+    public double getMonthlyPayment() {
+        return monthlyPayment;
+    }
+
+    public void setMonthlyPayment(double monthlyPayment) {
+        this.monthlyPayment = monthlyPayment;
+    }
+
+    public String getPaymentDate() {
+        return paymentDate;
+    }
+
+    public void setPaymentDate(String paymentDate) {
+        this.paymentDate = paymentDate;
+    }
+}
+
+
---
+++ src/domain/Transaction.java
@@ -0,0 +1,61 @@
+package domain;
+
+public class Transaction {
+
+    private int transactionID;
+    private String transactionDate;
+    private int accountNumber;   //this is a foreign from customer!!! no solve now
+    private String description;
+    private int transactionAmount;
+
+    public Transaction(int transactionID, String transactionDate, int accountNumber, String description, int transactionAmount) {
+        this.accountNumber = accountNumber;
+        this.transactionID = transactionID;
+        this.transactionDate = transactionDate;
+        this.accountNumber = accountNumber;   //this is a foreign from customer!!! no solve now
+        this.description = description;
+        this.transactionAmount = transactionAmount;
+    }
+
+    public int getTransactionID() {
+        return transactionID;
+    }
+
+    public String getTransactionDate() {
+        return transactionDate;
+    }
+
+    public int getAccountNumber() {
+        return accountNumber;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public int getTransactionAmount() {
+        return transactionAmount;
+    }
+
+    public void setTransactionID(int transactionID) {
+        this.transactionID = transactionID;
+    }
+
+    public void setTransactionDate(String transactionDate) {
+        this.transactionDate = transactionDate;
+    }
+
+    public void setAccountNumber(int accountNumber) {
+        this.accountNumber = accountNumber;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public void setTransactionAmount(int transactionAmount) {
+        this.transactionAmount = transactionAmount;
+    }
+    
+
+}
